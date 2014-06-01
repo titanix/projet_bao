723fb21cc966b4190a950c89faaa4794e1484f21
Louis Lecailliez <louis.lecailliez@outlook.fr>
2014-06-02 01:54:34 +0200

    Extract_pattern.pl amélioré ; code final.

--- bao3/extract_pattern.pl
+++ bao3/extract_pattern.pl
@@ -1,16 +1,56 @@
 use strict;
 use warnings;
+use Getopt::Long;
 
-my $filename = "test.txt";
+my $filename = '<pas de fichier>'; # chemin du fichier à traiter
+my $patt_file = 'patterns.txt'; # chemin du fichier de motif
+my $use_cordial = 0; # utilise l'ordre des fichiers générés par Cordial
+my $help = 0; # affiche l'aide ou non
 
-if(!open(PAT_FILE, "<:encoding(UTF-8)", "patterns.txt")) { die $! ; }
+my $help_text = "usage : perl parcours.pl --input_dir=<dossier_entrée> [-]\n";
+
+$help_text = <<END;
+usage : perl extract_pattern.pl --file=<fichier_à_traiter> [options]
+
+Les options suivantes sont disponibles (formes longues puis courtes) :
+--patterns		-p Nom du fichier contenant les motifs à utiliser.
+--cordial		-c À utiliser pour traiter un fichier taggé par Cordial.
+--help			-h Affiche ce message d'aide et quitte le programme.
+END
+
+my $res = GetOptions (
+	'file=s' => \$filename, 
+	'patterns:s' => \$patt_file,
+	'cordial' => \$use_cordial, 
+	'help' => \$help);
+	
+if(! -f $filename)
+{
+	print "Erreur le fichier suivant est introuvable : $filename\n";
+	exit 1;
+}
+
+if($help == 1)
+{
+	print $help_text;
+	exit 0;
+}
+
+# valeurs par défaut pour TreeTagger
+my $word_pos = 0; # position du mot non lemmatisé dans une ligne de données
+my $type_pos = 1; # position de l'indication de type dans une ligne de données
+
+if($use_cordial == 1)
+{
+	$word_pos = 0;
+	$type_pos =	2;
+}
+
+if(!open(PAT_FILE, "<:encoding(UTF-8)", $patt_file)) { die $! ; }
 if(!open(DATA_FILE, $filename)) { die $! ; }
-#if(!open(DATA_FILE, "<:encoding(UTF-8)", $filename)) { die $! ; }
 
 my @patterns = ();
 my @data = ();
-my $word_pos = 0; # position du mot non lemmatisé dans une ligne de données
-my $type_pos = 1; # position de l'indication de type dans une ligne de données
 
 while(<PAT_FILE>)
 {
@@ -23,13 +63,14 @@ while (<DATA_FILE>) {
 	if ($_ =~ /^\S+\s+\S+\s+\S+$/) # on vérifie que les données sont conformes à nos attentes
 	{
 		$_ =~ s/\R//;
-		push(@data, [split(/\s+/)]);
+		push(@data, [split(/\s+/)]); # the creepier, the better
 	}
 }
 
 # liste contenant, pour chaque motif, les chaînes de caractères correspondantes trouvées
 my @result = ();
-# liste temporaire qui contient la suite partielle de (type?) qui correspondent au motif courant
+# liste temporaire qui contient la suite partielle de catégories syntaxiques
+# qui correspondent au motif courant
 my @temp = ();
 # j'utilise la suite i, k, m pour les boucles plutôt que i, j, k car j ressemble trop à i
 my ($i, $k, $m, $r);
@@ -49,7 +90,7 @@ for ($m = 0;  $m < scalar(@patterns); $m++, $r = 0) # pour chaque motif
                 @temp = ();
                 last; # break
             }
-            if ($k == (scalar(@{$patterns[$m]}) - 1)) # quand on arrives à la fin du motif
+            if ($k == (scalar(@{$patterns[$m]}) - 1)) # quand on arrive à la fin du motif
             {
 				$result[$m][$r++] = join(' ', @temp);
                 @temp = ();
@@ -76,4 +117,3 @@ for ($i = 0; $i < scalar(@result); $i++) # pour tout les motifs
 		print "* Aucun résultat pour le motif @{$patterns[$i]}\n";
 	}
 }
-
